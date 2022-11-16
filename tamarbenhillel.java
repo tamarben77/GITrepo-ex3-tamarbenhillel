bfe9d05aaca7d28078de6c40409e953d18bc905f
tamarben77 <tamarben77@gmail.com>
2022-11-16 21:33:53 +0200

    new new new hello

--- tamarbenhillel.java
+++ tamarbenhillel.java
@@ -1,7 +1,7 @@
 public class tamarbenhillel { 
 	public static void main(String[] args){
 		for (int i=0; i<=10;i++){
-		System.out.println("Hello new print"+1);
+		System.out.println("Hello new print print"+1);
 		}
 	}
 }
